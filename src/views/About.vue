a88873fe7d5db3ee2fd0bc9b0868e0cbebcd3707
aburnard <36141001+aburnard@users.noreply.github.com>
2020-10-19 10:50:45 +1100

    Made pixmatrix a bit smaller
    some other draft stuff

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,6 +9,7 @@
     <section>
       <b-button @click="dumpButton">Dump</b-button>
       <b-button @click="loadButton">Load</b-button>
+      <b-button @click="newCanvas">newCanvas</b-button>
     </section>
     <div class="field">
       <b-switch v-model="allVisible">Default</b-switch>
@@ -16,7 +17,14 @@
     <section>
       <b-button @click="dumpIt()">Download as SVG</b-button>
     </section>
-    <h1>IsoPixelDraw Zoom {{xBias}} Size {{Size}} vbx {{vbx}} vby {{vby}} vbWidth {{vbWidth}} vbHeight {{vbHeight}}</h1>
+    <button class="button is-medium is-dark" @click="promptNumber">
+      Launch prompt (number)
+    </button>
+    <h1>
+      IsoPixelDraw Zoom {{ xBias }} Size {{ Size }} vbx {{ vbx }} vby
+      {{ vby }} vbWidth {{ vbWidth }} vbHeight {{ vbHeight }} sizebyzoom
+      {{ Size * xBias }}
+    </h1>
     <b-field>
       <vswatches v-model="color" swatches="text-advanced"></vswatches>
     </b-field>
@@ -45,46 +53,57 @@
         <b-slider :min="500" :max="5000" v-model="vbHeight"></b-slider>
       </b-field>
     </section>
-
-    <div class="container is-fluid" v-if="allVisible">
-      <svg
-        id="isoSvg"
-        ref="isoSvg"
-        :viewBox="computedViewBox"
-        :width="xBias * Size * 1.2"
-        :height="xBias * Size"
-      >
-        <!-- p in size is y coord -->
-        <!-- n in size is x coord -->
-        <g v-for="p in pixMatrix[1].length" :key="p" width="100%" :height="xBias * .65">
-          <ComponentPoly
-            :color="color"
-            v-for="n in pixMatrix[0].length"
-            :id="key * Math.floor(Math.random() * 10)* Math.floor(Math.random() * 10) * Math.floor(Math.random() * 10)"
-            :key="n"
-            :pixMatrix="pixMatrix"
-            :xBias="n * xBias + (p%2 * xBias/2)"
-            :yBias="p*xBias"
-            :sideLength="xBias"
-            :yCoord="p-1"
-            :xCoord="n-1"
-            @colorPixel="colorPixel"
-          />
-          <ComponentPolyUp
-            :id="key * Math.floor(Math.random() * 10)* Math.floor(Math.random() * 10) * Math.floor(Math.random() * 10)"
-            :color="color"
-            v-for="n in pixMatrixUp[0].length"
-            :key="n"
-            :xBias="n * xBias + (p%2 * xBias/2)"
-            :pixMatrixUp="pixMatrixUp"
-            :yBias="p*xBias"
-            :sideLength="xBias"
-            :yCoord="p-1"
-            :xCoord="n-1"
-            @colorPixelUp="colorPixelUp"
-          />
-        </g>
-      </svg>
+    <div class="box">
+      <div class="container is-fluid" v-if="allVisible">
+        <svg
+          id="isoSvg"
+          ref="isoSvg"
+          :viewBox="computedViewBox"
+          :width="xBias * Size * 1.2"
+          :height="xBias * Size"
+        >
+          <!-- p in size is y coord -->
+          <!-- n in size is x coord -->
+          <g v-for="p in 30" :key="p" width="100%" :height="xBias * 0.65">
+            <ComponentPoly
+              :color="color"
+              v-for="n in 30"
+              :id="
+                key *
+                  Math.floor(Math.random() * 10) *
+                  Math.floor(Math.random() * 10) *
+                  Math.floor(Math.random() * 10)
+              "
+              :key="n"
+              :pixMatrix="pixMatrix"
+              :xBias="n * xBias + ((p % 2) * xBias) / 2"
+              :yBias="p * xBias"
+              :sideLength="xBias"
+              :yCoord="p - 1"
+              :xCoord="n - 1"
+              @colorPixel="colorPixel"
+            />
+            <ComponentPolyUp
+              :id="
+                key *
+                  Math.floor(Math.random() * 10) *
+                  Math.floor(Math.random() * 10) *
+                  Math.floor(Math.random() * 10)
+              "
+              :color="color"
+              v-for="n in 30"
+              :key="n"
+              :xBias="n * xBias + ((p % 2) * xBias) / 2"
+              :pixMatrixUp="pixMatrixUp"
+              :yBias="p * xBias"
+              :sideLength="xBias"
+              :yCoord="p - 1"
+              :xCoord="n - 1"
+              @colorPixelUp="colorPixelUp"
+            />
+          </g>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -115,11 +134,11 @@ export default {
       fileSaveObject: {},
       pixMatrixString: "",
       color: "red",
-      xBias: 98,
-      Size: 5,
+      xBias: 42,
+      Size: 29,
       allVisible: true,
-      vbx: 75,
-      vby: 405,
+      vbx: 0,
+      vby: 230,
       vbWidth: 1368,
       vbHeight: 623,
       viewBoxArray: [],
@@ -171,11 +190,46 @@ export default {
         this.downer = true;
       }
     },
+    promptNumber() {
+      this.$buefy.dialog.prompt({
+        message: `What's your age?`,
+        inputAttrs: [
+          {
+            type: "number",
+            placeholder: "Type your age",
+            value: "18",
+            maxlength: 2,
+            min: 18
+          },
+          {
+            type: "number",
+            placeholder: "Type your age",
+            value: "18",
+            maxlength: 2,
+            min: 18
+          }
+        ],
+
+        trapFocus: true,
+        onConfirm: value => this.$buefy.toast.open(`Your age is: ${value}`)
+      });
+    },
     applied() {
       this.downer = "true";
     },
     released() {
       this.downer = "false";
+    },
+    newCanvas() {
+      var A = [];
+      for (var i = 0; i < 40; i++) {
+        A[i] = [];
+        for (var j = 0; j < 50; j++) {
+          A[i][j] = "blue";
+        }
+      }
+      this.pixMatrixUp = A.slice(0);
+      this.pixMatrix = A.slice(0);
     },
     colorPixel(recievedCoords) {
       //this.matrixPlaceHolder = this.pixMatrix.slice(0);
